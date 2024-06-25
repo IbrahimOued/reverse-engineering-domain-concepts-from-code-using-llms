9f2b556ffaed7cdc5884379704bb9c07391a9f19
Ibrahim Ouedraogo <ibrahim.ouedraogo@umontreal.ca>
2024-06-25 15:13:11 -0400

    build: replace the default prompt by suggestion prompt

--- llm_helper.py
+++ llm_helper.py
@@ -9,7 +9,7 @@ class LLMHelper:
     # TODO actually class context is class name
     def generate_similar_concepts(class_concept, llm, model, log_time):
         # import the prompt config file
-        with open(config['prompts']['default']) as f:
+        with open(config['prompts']['suggestion']) as f:
             json_prompt = json.load(f)
             context = json_prompt['context']
             prompt = json_prompt['prompt']
@@ -34,6 +34,20 @@ class LLMHelper:
 
             # create a log file to store the class candidate generated
             log_file = open(f"./artifacts/chat_logs/{log_time}.log", "a")
+            log_file.write(f">[user]: {prompt}\n>[{llm}]: {response}\n\n\n")
+            log_file.close()
+            return response
+
+    def justify_suggestion(class_str, llm, model, log_time):
+        with open(config['prompts']['justification']) as f:
+            json_prompt = json.load(f)
+            prompt = json_prompt['prompt']
+            prompt = prompt.replace("{{class}}", class_str)
+            llm_service = LLMService.init_llm(llm, model)
+            response = llm_service.generate_llm_response(context=class_str, prompt=prompt)
+
+            # create a log file to store the class candidate generated
+            log_file = open(f"./artifacts/justification_logs/{log_time}.log", "a")
             log_file.write(f">[user]: {prompt}\n>[{llm}]: {response}\n\n\n")
             log_file.close()
             return response
